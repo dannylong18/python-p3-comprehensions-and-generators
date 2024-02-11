1559a5ece9c9e5c420fd7662ef134ef3d3b15129
Daniel Long <danielwlong618@gmail.com>
2024-02-11 16:53:25 -0600

    lab done

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [item for item in num_list if item % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [item + '!' for item in sentence_list]
